af6c0a5952f848ce38326915afa505ebbff1be5f
Neer Friedman <neer@neer-laptop.(none)>
2009-03-08 13:11:07 +0200

    load configuration on init

--- init.rb
+++ init.rb
@@ -8,3 +8,8 @@ relative_require 'lib/contacts/flickr'
 relative_require 'lib/contacts/google'
 relative_require 'lib/contacts/windows_live'
 relative_require 'lib/contacts/yahoo'
+
+#if RAILS_ROOT/config/contacts.yml exists load it, if not load the local config file
+app_config = File.join(File.dirname(__FILE__), '../../../config/contacts.yml')
+plugin_config = File.join(File.dirname(__FILE__), 'config/contacts.yml')
+Contacts.load_config_from_file(File.exists?(app_config) ? app_config : plugin_config)
