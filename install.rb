4e2789d43c8144bfe3c7d6653ac2bcfd41553dc4
Neer Friedman <neer@neer-laptop.(none)>
2009-03-08 11:48:04 +0200

    more simple install.rb

--- install.rb
+++ install.rb
@@ -1,4 +1,4 @@
 current_dir = File.dirname(__FILE__)
 
-print "Copying contacts.yml file to config directory..."
+puts "Copying contacts.yml file to config directory..."
 FileUtils.cp File.join(current_dir,'config/contacts.yml'), File.join(RAILS_ROOT, 'config')
